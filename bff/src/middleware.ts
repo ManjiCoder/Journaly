3fc6f56941b88e2fd82bac1bc8bf44aafb9cf96b
Manji Coder <manjicoder@gmail.com>
2025-08-08 16:00:07 +0530

    middleware

--- bff/src/middleware.ts
+++ bff/src/middleware.ts
@@ -19,66 +19,67 @@ const verifyUser = async (req: NextRequest) => {
   }
 };
 export async function middleware(req: NextRequest) {
-  // Public API
-  const url = req.nextUrl;
+  // const url = req.nextUrl;
   const origin = req.headers.get('origin');
   const whileListOrigin = ['http://localhost:5173'];
   const { method } = req;
 
-  if (url.pathname === '/api/hello') {
-    const res = NextResponse.next();
-    res.headers.set('Access-Control-Allow-Origin', '*');
-    res.headers.set('Access-Control-Allow-Methods', 'GET');
-    return res;
+  // Handle preflight
+  if (method === 'OPTIONS') {
+    return new NextResponse(null, {
+      status: 204,
+      headers: {
+        'Access-Control-Allow-Origin': whileListOrigin.includes(origin || '')
+          ? origin!
+          : '',
+        'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE',
+        'Access-Control-Allow-Headers': 'Content-Type, Authorization',
+      },
+    });
   }
-  // Private API
-  else {
-    // Handle preflight
-    if (method === 'OPTIONS') {
-      return new NextResponse(null, {
-        status: 204,
-        headers: {
-          'Access-Control-Allow-Origin': whileListOrigin.includes(origin || '')
-            ? origin!
-            : '',
-          'Access-Control-Allow-Methods': 'GET, POST, PATCH, PUT, DELETE',
-          'Access-Control-Allow-Headers': 'Content-Type, Authorization',
-        },
-      });
-    }
-    const isDevMode =
-      req.headers.get('user-agent') ===
-      'Thunder Client (https://www.thunderclient.com)';
 
-    // Block unknown origins
-    if (!origin || !whileListOrigin.includes(origin)) {
-      if (!isDevMode) {
-        return new NextResponse(
-          JSON.stringify({
-            success: false,
-            message: 'Forbidden Access',
-          }),
-          { status: 403 }
-        );
-      }
+  // Block unknown origins
+  if (!origin || !whileListOrigin.includes(origin)) {
+    const token = req.headers
+      .get('authorization')
+      ?.replace('Bearer ', '') as string;
+    if (!token) {
+      return new NextResponse(
+        JSON.stringify({
+          success: false,
+          message: 'Please autheticate using a valid token',
+        }),
+        { status: 401 }
+      );
     }
+    const payload = await verifyToken(token, myConfig.JWT_KEY);
+    // console.log(payload);
 
-    if (origin) {
-      const res = NextResponse.next();
-      res.headers.set('Access-Control-Allow-Origin', origin);
-      res.headers.set(
-        'Access-Control-Allow-Methods',
-        'GET, POST, PATCH, PUT, DELETE'
+    // res.headers.set('x-id', payload.id);
+    if (!payload) {
+      return new NextResponse(
+        JSON.stringify({
+          success: false,
+          message: 'Invalid token',
+        }),
+        { status: 401 }
       );
-      res.headers.set(
-        'Access-Control-Allow-Headers',
-        'Content-Type, Authorization'
-      );
-      return res;
     }
   }
+
+  const res = NextResponse.next();
+  res.headers.set('Access-Control-Allow-Origin', origin!);
+  res.headers.set(
+    'Access-Control-Allow-Methods',
+    'GET, POST, PATCH, PUT, DELETE'
+  );
+  res.headers.set(
+    'Access-Control-Allow-Headers',
+    'Content-Type, Authorization'
+  );
+  return res;
 }
 
 export const config = {
-  matcher: ['/api/:path*'],
+  matcher: ['/api/dairy'],
 };
